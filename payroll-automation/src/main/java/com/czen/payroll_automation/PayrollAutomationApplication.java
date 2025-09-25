a176e8e546ea192770c5a343bf2aaa29f23dbe6b
mlukenich <73347955+mlukenich@users.noreply.github.com>
2025-09-25 18:11:24 -0400

    initial skeleton spring

---
+++ payroll-automation/src/main/java/com/czen/payroll_automation/PayrollAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.czen.payroll_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollAutomationApplication.class, args);
+	}
+
+}
